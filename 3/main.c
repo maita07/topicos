467053fd9894a1102f9904f751cbfa75150a63fb
maita07 <josemaitapitado@gmail.com>
2023-07-25 00:16:17 -0300

    hola

--- 3/main.c
+++ 3/main.c
@@ -1,22 +1,70 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/* imprime la tabla Fahrenheit-Celsius
-para fahr = 0, 20, ..., 300 */
-int main ( )
+#define PI 3.14
+
+#define ABSOLUTO(x) ((x) < 0 ? -(x) : (x))
+
+
+
+double factorial(int n);
+float raizCuadradaDeA(int a, float tol);
+int perteneceFibo(int n);
+
+int main(void)
 {
-    float fahr, celsius;
-    int lower, upper, step;
-    lower = 0 ; /* límite inferior de la tabla de temperaturas */
-    upper = 300; /* límite superior */
-    step = 20; /* tamaño del incremento */
+    float raiz = raizCuadradaDeA(2, 0.00001);
+    printf("\n%d\n\n", ABSOLUTO(-4));
+    printf("\n%.9f\n\n", raiz);
 
-    fahr = lower;
 
-    while (fahr <= upper)
+
+    return perteneceFibo(21);
+}
+
+
+double factorial(int n)
+{
+    int multiplicador;
+    for(multiplicador = n-1; multiplicador > 1; multiplicador--)
     {
-        celsius = 5.0 * (fahr-32.0) / 9.0;
-        printf("%3.0f %6.1f\n", fahr, celsius);
-        fahr = fahr + step;
+        n*=multiplicador;
     }
+
+    return n;
+}
+
+float raizCuadradaDeA(int a, float tol)
+{
+    float R1 = 1;
+    float Ri = 0.5 * (R1 + (a/R1));
+
+    while(ABSOLUTO(R1-Ri) > tol)
+    {
+        R1 = Ri;
+        Ri = 0.5 * (R1 + (a/R1));
+    }
+
+    return Ri;
+}
+
+int perteneceFibo(int n)
+{
+    int n1 = 0, n2 = 1, n3;
+
+    if(n == 1)
+        return 1;
+
+    while((n1 + n2) < n)
+    {
+        n3 = n1 + n2;
+        n1 = n2;
+        n2 = n3;
+    }
+
+    if(n1 + n2 == n)
+    {
+        return 1;
+    }
+    else{return 0;}
 }
